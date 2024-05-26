ae73eff57222e2303cf2648b787514271f74717c
Felix CADENE <felixcadene0@gmail.com>
2024-05-26 21:50:20 +0200

    Changement de tt document en .php pour favoriser le dynamique a 100%
    Ajout du Header
    Le Header.html est le header que l'on verra sur tt les pages. Dessus, on verra le titre du site (clickable, qui renvoit a l'acceuil), le logo du site (EngineerIN)
    Il y a aussi la bar de navigation, soit les cing boutons qui vont envoyer l'utilisateur a six fichiers .php differents.
    
    Normalisation de la mise en page. Fond blanc-gris.
    Tout le contenu du site sera sur un bloc ("main_bloc") de fond blanc.
    
    Pour chaque fichier .php que l'utilisateur verra, il faudra etre sur cette estetique pour garder une coherence.
    
    Le but de ce main_bloc: SE RESSEMBLER AU MAX A LINKEDIN.

---
+++ profile_main.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+    <title>Profile</title>
+    <style>
+        body
+        {
+            background-color: #d8d8d8;
+        }
+        #main_bloc 
+        {
+            width: 100%;
+            background-color: white;
+            color: white;
+            text-align: center;
+        }
+    </style>
+
+
+</head>
+
+<body>
+    <?php include 'Header.html';?>
+
+   
+
+    <div class="container" id="background">
+        <div class="container" id="main_bloc">
+            <h1 style="color: black;">Profile</h1>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
