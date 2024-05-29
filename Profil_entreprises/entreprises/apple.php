11085b962cb3f0c41cdb112176d0b38642c6f77e
Annabelle Leoni <annabelle.leoni@edu.ece.fr>
2024-05-29 11:48:26 +0200

    en cours de réalisation de l'entreprise

--- Profil_entreprises/entreprises/apple.php
+++ Profil_entreprises/entreprises/apple.php
@@ -1,14 +1,6 @@
 <?php
-        $menu_items = [
-        "Accueil" => "#",
-        "A propos" => "#",
-        "Produits" => "#",
-        "Posts" => "#",
-        "Emplois" => "#",
-        ];
-$current_page = "Accueil"; // Change this variable based on the current page
+    include '../../Profils_entreprises/details/accueil.php';
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -18,39 +10,19 @@ $current_page = "Accueil"; // Change this variable based on the current page
     <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+
     <link rel="stylesheet" type="text/css" href="details.css">
     <script>
         function showText() {
             alert("Vous avez cliqué sur Accueil !");
         }
-        </script>
-    <script>
-
-        function accueil() {
-            document.getElementById("content").innerHTML = `
-                <div class="container" id="background" style="padding : 15px">
-                    <div class="container" id="main_bloc">
-                        <div class="row">
-                            <br><br>
-                            <h1 style="margin:15px">Informations sur l'entreprise</h1>
-                            <br>
-                            <p style="margin: 15px">
-                                We’re a diverse collective of thinkers and doers, continually reimagining what’s possible to help us all do what we love in new ways. And the same innovation that goes into our products also applies to our practices — strengthening our commitment to leave the world better than we found it. This is where your work can make a difference in people’s lives. Including your own.
-                                <br><br>
-                                Apple is an equal opportunity employer that is committed to inclusion and diversity. Visit apple.com/careers to learn more.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            `;
-        
-        }
-
     </script>
+    
 </head>
 
 <body>
     
+
 <div class="container" id="background"> 
     <div class="container" id="main_bloc">
         <div class="row">
@@ -62,24 +34,44 @@ $current_page = "Accueil"; // Change this variable based on the current page
 
         <button onclick="window.location.href='https://www.apple.com/careers/us/'">Consulter le site web</button><br><br>
 
+        
 
     <div class="nav-bar">
-        <?php foreach ($menu_items as $name => $link): ?>
-            <a href="<?php echo $link; ?>" class="nav-item <?php echo $name == $current_page ? 'active' : ''; ?>"
-            <?php echo $name == "Accueil" ?  onclick=accueil() : ''; ?>
-            <?php echo $name == "A propos" ? onclick=showText() : ''; ?>
-            <?php echo $name == "Produits" ? onclick=showText() : ''; ?>
-            <?php echo $name == "Posts" ?    onclick=showText() : ''; ?>
-            <?php echo $name == "Emplois" ?  onclick=showText() : ''; ?>>
-                <?php echo $name; ?>
-            </a>
-        <?php endforeach; ?>
-      
-    </div>
-    </div>
-   
-</div>
+        <form method="post" action="">
+            <input type="hidden" name="OngletNavBar" value="Accueil">
+            <button type="submit">Accueil</button>
+        </form>
+        <form method="post" action="">
+            <input type="hidden" name="OngletNavBar" value="A propos">
+            <button type="submit">A propos</button>
+        </form>
+        <form method="post" action="">
+            <input type="hidden" name="OngletNavBar" value="Posts">
+            <button type="submit">Posts</button>
+        </form>
+        <form method="post" action="">
+            <input type="hidden" name="OngletNavBar" value="Emplois">
+            <button type="submit">Emplois</button>
+        </form>
 
+
+        <?php 
+
+            if isset($_POST['OngletNavBar']) {
+                $current_page = $_POST['OngletNavBar'];
+            }
+            else {
+                $current_page = "Accueil";
+            }
+
+            
+            //$sql = "SELECT Content FROM Entreprises WHERE Nom = 'Apple'" AND "Onglet = $current_page";
+        ?>
+        
+    </div>
+    </div>
+
+</div>
 
 </body>
 </html>
---
+++ Profil_entreprises/entreprises/apropos.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <title>bannière</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+
+    <link rel="stylesheet" type="text/css" href="details.css">
+</head>
+<body>
+
+
+<div class="container" id="background" style="padding : 15px">
+<div class="container" id="main_bloc">
+    <div class="row">
+        <br><br><br>
+        <h1 style ="margin:15px">Présentation</h1>
+        <p style ="margin:15px">We’re a diverse collective of thinkers and doers, continually reimagining what’s possible to help us all do what we love in new ways. And the same innovation that goes into our products also applies to our practices — strengthening our commitment to leave the world better than we found it. This is where your work can make a difference in people’s lives. Including your own.</p>
+        <br><br>
+        <p style ="margin:15px">Apple is an equal opportunity employer that is committed to inclusion and diversity. Visit happle.com/careers to learn more. </p>
+    </div>
+
+    <br><br>
+        <h5>Site Web</h5>
+        <a href="http://www.apple.com/careers" >apple.com/careers</a><br><br>
+
+        <h5>Secteur</h5>
+        <p>Fabrication de produits informatiques et électroniques</p><br>
+        <h5>Taille de l'entreprise</h5>
+        <p>10 001 employés et plus</p>
+        <p>171 475 membres associés Membres associés</p><br>
+        <h5>Siège social</h5>
+        <p>Cupertino, California</p><br>
+        <h5>Année de création</h5>
+        <p>1976</p><br>
+        <h5>Spécialisations</h5>
+        <p>Innovative Product Development, World-Class Operations, Retail et Telephone Support</p>
+        <br><br>   
+
+
+
+        LIEUUUUUUUU
+        
+        </div></div></div>
+
+
+
+</body>
+
+</html>
