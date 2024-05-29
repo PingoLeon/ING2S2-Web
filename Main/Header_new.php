f22140d0f65ae97c4c8dd2e813eb0775cf1bb6ff
Felix CADENE <felixcadene0@gmail.com>
2024-05-30 01:00:41 +0200

    new header

--- Main/Header_new.php
+++ Main/Header_new.php
@@ -13,156 +13,154 @@
     
     <style>
         * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: sans-serif;
-    }
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
 
-    body {
-        width: 100%;
-        height: 100vh;
-        background: rgb(247, 242, 242);
-    }
+        body {
+            width: 100%;
+            height: 100vh;
+            background: rgb(247, 242, 242);
+        }
 
-    header {
-        height: 60px;
-        width: 100%;
-        background: #fff;
-        box-shadow: 0px 2px 5px 5px rgba(204, 202, 202, 0.5);
-        display: flex;
-        align-items: center;
-    }
+        header {
+            height: 80px; /* Increase the height of the header */
+            width: 100%;
+            background: #fff;
+            box-shadow: 0px 2px 5px 5px rgba(204, 202, 202, 0.5);
+            display: flex;
+            align-items: center;
+        }
 
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 92%;
-        margin: 0 auto;
-    }
+        nav {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 92%;
+            margin: 0 auto;
+        }
 
-    .nav-left .logo img {
-        width: 200px;
-        height: auto;
-        margin-left: -10px;
-    }
+        .nav-left .logo img {
+            width: 220px; /* Increase the width of the logo */
+            height: auto;
+            margin-left: -10px;
+        }
 
+        .nav-left .search-icon {
+            width: 35px; /* Increase the size of the search icon */
+            text-align: center;
+            color: rgb(44, 42, 42);
+        }
 
-    .nav-left .search-icon {
-        width: 30px;
-        text-align: center;
-        color: rgb(44, 42, 42);
-    }
+        .nav-right {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            flex: 1;
+            margin-left: 100px;
+        }
 
-    .nav-right {
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        flex: 1;
-    }
+        .nav-right .nav-menus {
+            text-align: center;
+            cursor: pointer;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding: 0 15px; /* Increase padding for more spacing */
+            position: relative;
+        }
 
-    .nav-right .nav-menus {
-        text-align: center;
-        cursor: pointer;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        padding: 0 10px;
-        position: relative;
-    }
+        .nav-right .nav-menus i {
+            font-size: 25px; /* Increase the size of the icons */
+            color: #706e6e;
+            margin-bottom: 3px;
+        }
 
-    .nav-right .nav-menus i {
-        font-size: 20px;
-        color: #706e6e;
-        margin-bottom: 2px;
-    }
+        .nav-right .nav-menus p {
+            font-size: 16px; /* Increase the font size of the text */
+            color: #706e6e;
+            margin: 0;
+        }
 
-    .nav-right .nav-menus p {
-        font-size: 13px;
-        color: #706e6e;
-        margin: 0;
-    }
+        .nav-right .nav-menus:nth-child(7) {
+            border-left: 1px solid #ccc;
+            padding-left: 15px;
+        }
 
-    .nav-right .nav-menus:nth-child(7) {
-        border-left: 1px solid #ccc;
-        padding-left: 15px;
-    }
+        .sponsor a {
+            font-size: 16px;
+            color: rgb(185, 122, 5);
+            margin-right: -60px;
+            text-decoration: none;
+        }
 
-    .nav-right a {
-        font-size: 13px;
-        color: rgb(185, 122, 5);
-        padding-left: 15px;
-        text-decoration: none;
-    }
-
-
-    .nav-right .nav-menus:hover::after {
-        content: '';
-        width: 100%;
-        height: 2px;
-        background: #000;
-        position: absolute;
-        bottom: -10px;
-        left: 0;
-    }
-
-
-
-
+        .nav-right .nav-menus:hover::after {
+            content: '';
+            width: 100%;
+            height: 2px;
+            background: #000;
+            position: absolute;
+            bottom: -10px;
+            left: 0;
+        }
     </style>
 </head>
 
-
-
 <body>
     <header>
-
         <nav>
             <div class="nav-left">
                 <div class="logo">
-                    <img src="../Photos/EngineerIN_logo.png" alt="ECE Paris"></img>
+                    <img src="../Photos/EngineerIN_logo.png" alt="ECE Paris">
                 </div>
-                
             </div>
             <div class="nav-right">
                 <div class="nav-menus">
-                    <i class="fa-solid fa-house"></i>
-                    <p>Home</p>
+                    <a href="accueil_main.php">
+                        <i class="fa-solid fa-house"></i>
+                        <p>Home</p>
+                    </a>
                 </div>
                 <div class="nav-menus">
+                    <a href="../MonReseau/MonReseau.html">
                     <i class="fa-solid fa-users"></i>
                     <p>Mon Reseau</p>
+                    </a>
                 </div>
-
                 <div class="nav-menus">
+                    <a href="../Emplois/Emplois.html">
                     <i class="fa-solid fa-briefcase"></i>
                     <p>Emplois</p>
+                    </a>
                 </div>
-
                 <div class="nav-menus">
+                    <a href="../Messagerie/index.php">
                     <i class="fa-solid fa-message"></i>
                     <p>Messagerie</p>
+                    </a>
                 </div>
                 <div class="nav-menus">
+                    <a href="../Notifications/Notifications.html">
                     <i class="fa-solid fa-bell"></i>
                     <p>Notification</p>
+                    </a>
                 </div>
-
                 <div class="nav-menus">
+                    <a href="../Profile/profile_main.php">
                     <i class="fa-solid fa-user"></i>
-                    <p>Vous </p>
+                    <p>Vous</p>
+                    </a>
                 </div>
                 <div class="nav-menus">
                     <i class="fa-solid fa-border-none"></i>
                     <p>For Business <i class="fa-solid fa-caret-down"></i></p>
                 </div>
-
-                <a href="">Try Premium <br>for $100</a>
+                <a class="sponsor" href=""><b>Sponsorisez notre<br>futur Engineering+ !!<b></a>
             </div>
         </nav>
-
     </header>
 </body>
 
--- Profile/profile_skills.php
+++ Profile/profile_skills.php
@@ -1,44 +1,51 @@
-<!-- ProfileTitle.php -->
-<?php
-$database = "ecein";
-$db_handle = mysqli_connect('localhost', 'root', '');
-$db_found = mysqli_select_db($db_handle, $database);
+<!DOCTYPE html>
+<html lang="en">
 
-$user_id = 1;
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-$sql = "SELECT * FROM utilisateur WHERE User_ID = '$user_id'";
-$result = mysqli_query($db_handle, $sql);
-$data = mysqli_fetch_assoc($result);
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: 'Poppins', 'sans-serif';
+            box-sizing: border-box;
+        }
 
-$prenom = $data['Prenom'];
-$nom = $data['Nom'];
-$email = $data['Mail'];
-$pays = $data['Pays'];
-$photo = $data['Photo'];
-$admin = $data['Statut_Admin'];
+        .hero{
+            width:100%;
+            min-height: 100vh;
+            background:#eceaff;
+            color: #333;
+        }
 
-echo '<div class="container" id="main_bloc_profile">';
-    echo '<br>';
-    echo '<div class="row">';
-        echo '<div class="col-md-6">';
-        echo '<img src="../' . $photo . '" class="rounded-circle" alt="Photo de profil" width="304" height="304">';
-        echo '</div>';
-        echo '<div class="col-md-5" style="margin-top:auto; margin-bottom:auto;">';
-            echo '<table>';
-            echo '<tr><h1 style="color: black;">' . $prenom . ' ' . $nom . '</h1></tr>';
-            echo '<tr><h3 style="color: black;">' . $email . '</h3></tr>';
-            echo '<tr><h3 style="color: black;">' . $pays . '</h3></tr>';
-            echo '<br>';
-            echo '<tr><a href="../Profile/CV.php" class="btn btn-primary">Générer un CV</a></tr>';
-            echo '</table>';
-        echo '</div>';
-        echo '<div class="col-md-1">';
-            //Modification du profil
-            echo '<a href="Modification.php?id=title" id="title">';
-            echo '<img src="../Photos/edit.png" alt="Modifier" width="40" height="40" >';
-            echo '</a>';
-        echo '</div>';
-    echo '</div>';
-    echo '<br>';
-echo '</div>';
-?>
+        nav{
+            background: #1a1a1a;
+            width: 100%;
+            padding: 10px 10%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    </style>
+        
+</head>
+
+<body>
+
+<div class="hero">
+    <nav>
+        <img src="../Photos/EngineerIN_logo.png" class="logo" style="width:10%;">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <img src="../Photos/photo1.png" class="user-pic" style="width:10%;">
+    </nav>
+</div>
+
+</body>
+</html>
