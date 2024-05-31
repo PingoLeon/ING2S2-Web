a05deee6a97a3568a0b47babd9c781c48bc5bfe2
Annabelle Leoni <annabelle.leoni@edu.ece.fr>
2024-06-01 01:00:52 +0200

    🛠🛠

--- Profil_entreprises/details/posts1.php
+++ Profil_entreprises/details/posts1.php
@@ -1,12 +1,12 @@
 <?php
 //echo '$result = mysqli_query($db_handle, $sql);';
 $sql1 = 'SELECT * FROM events WHERE Enterprise_ID = '.$entre_id.' ORDER BY Date_publication DESC';
-$result1 = mysqli_query($db_handle, $sql1);
-$event = mysqli_fetch_assoc($result1);
+$result = mysqli_query($db_handle, $sql1);
+$event = mysqli_fetch_assoc($result);
 
 if (($event['Intitulé']!=NULL) && ($event['Début']!= '0000-00-00' )&&($event['Fin']!= '0000-00-00')&&($event['Texte']!=NULL)&&($event['Photo']!=NULL)&&($event['Date_publication']!=NULL)){
 
-    while ($event) {
+    foreach ($result as $event) {
         echo'<div class="container" id="background" style="padding: 15px">';
         echo'<div class="container" id="main_bloc">';
         echo'<div class="row">';
@@ -15,8 +15,8 @@ if (($event['Intitulé']!=NULL) && ($event['Début']!= '0000-00-00' )&&($event['
         echo '<h1 style="margin: 15px">' . $event['Intitulé'] . '</h1>';
         echo '<p style="margin: 15px">' . $event['Texte'] . '</p>';
         
-        if (($event['Début']!= '0000-00-00' )&&($even1['Fin']!= '0000-00-00')){
-            echo '<p style="margin: 15px">📍📅 Retrouvez-nous du ' . $event['Début'] . ' jusqu\'au ' . $event1['Fin'] . '.</p>';
+        if (($event['Début']!= '0000-00-00' )&&($event['Fin']!= '0000-00-00')){
+            echo '<p style="margin: 15px">📍📅 Retrouvez-nous du ' . $event['Début'] . ' jusqu\'au ' . $event['Fin'] . '.</p>';
         }
     
         echo '<img class="image" src="../Profil_entreprises/photo_events/' . $event['Photo'] . '" style="margin: auto; max-width: 100%; height: auto;">';
