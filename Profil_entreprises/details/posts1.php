31176196eac1815623470316ce414f5bbde66a61
Annabelle Leoni <annabelle.leoni@edu.ece.fr>
2024-05-31 19:44:43 +0200

    si aucune information sur un évènement, alors rien💞

--- Profil_entreprises/details/posts1.php
+++ Profil_entreprises/details/posts1.php
@@ -1,13 +1,12 @@
 <?php
 //echo '$result = mysqli_query($db_handle, $sql);';
-$sql = 'SELECT * FROM events WHERE Enterprise_ID = '.$entre_id.' ORDER BY Date_publication DESC';
-$result = mysqli_query($db_handle, $sql);
+$sql1 = 'SELECT * FROM events WHERE Enterprise_ID = '.$entre_id.' ORDER BY Date_publication DESC';
+$result1 = mysqli_query($db_handle, $sql1);
+$event = mysqli_fetch_assoc($result1);
 
+if (($event['Intitulé']!=NULL) && ($event['Début']!= '0000-00-00' )&&($event['Fin']!= '0000-00-00')&&($event['Texte']!=NULL)&&($event['Photo']!=NULL)&&($event['Date_publication']!=NULL)){
 
-
-     
-            
-    while ($event = mysqli_fetch_assoc($result)) {
+    while ($event) {
         echo'<div class="container" id="background" style="padding: 15px">';
         echo'<div class="container" id="main_bloc">';
         echo'<div class="row">';
@@ -15,14 +14,18 @@ $result = mysqli_query($db_handle, $sql);
         echo '<i><p style="color:grey "> Publié le '. $event['Date_publication'] . '</p></i><br>';
         echo '<h1 style="margin: 15px">' . $event['Intitulé'] . '</h1>';
         echo '<p style="margin: 15px">' . $event['Texte'] . '</p>';
-        echo '<p style="margin: 15px">📍📅 Retrouvez-nous du ' . $event['Début'] . ' jusqu\'au ' . $event['Fin'] . '.</p>';
-        echo '<img class="image" src="../../Profil_entreprises/photo_events/' . $event['Photo'] . '" style="margin: auto; max-width: 100%; height: auto;">';
+        
+        if (($event['Début']!= '0000-00-00' )&&($even1['Fin']!= '0000-00-00')){
+            echo '<p style="margin: 15px">📍📅 Retrouvez-nous du ' . $event['Début'] . ' jusqu\'au ' . $event1['Fin'] . '.</p>';
+        }
+    
+        echo '<img class="image" src="../Profil_entreprises/photo_events/' . $event['Photo'] . '" style="margin: auto; max-width: 100%; height: auto;">';
         
         echo'</div>';
         echo'</div>';
         echo'</div>';
+
     }
-
-
+}
 ?>
 <br><br><br>
--- Profil_entreprises/entreprise.php
+++ Profil_entreprises/entreprise.php
@@ -1,5 +1,5 @@
 <?php
-include 'C:\wamp64\www\ING2S2-Web/Auth/functions.php';
+include '../Auth/functions.php';
 //! Renvoyer l'utilisateur à la page de connexion si il n'est pas connecté, sinon récupérer l'id et l'email
 list($id, $email, $db_handle) = check_if_cookie_or_session_and_redirect_else_retrieve_id_mail_handle();
 $user_id = $id;
@@ -22,13 +22,17 @@ $user_id = $id;
     $sql = 'SELECT * FROM enterprise, informations, events WHERE enterprise.Information_ID = '.$entre_id.' AND informations.Information_ID = '.$entre_id.' AND events.Enterprise_ID = '.$entre_id.';';
     $result = mysqli_query($db_handle, $sql);
     $data = mysqli_fetch_assoc($result);
+    if (mysqli_num_rows($result) == 0) {
+        echo "Aucune entreprise trouvée";
+        exit;
+    }
 ?>
 
 
 <div class="container" id="background"> 
-    <div class="container" id="main_bloc" style="background-image: url('../../Profil_entreprises/bannieres/<?php echo $data['Banniere']; ?>');">
+    <div class="container" id="main_bloc" style="background-image: url('../Profil_entreprises/bannieres/<?php echo $data['Banniere']; ?>');">
         <div class="row">
-            <?php echo '<img id ="ban" class="image" src="../../Profil_entreprises/logos/'.$data['Logo'].'" alt="Logo">'; ?>
+            <?php echo '<img id ="ban" class="image" src="../Profil_entreprises/logos/'.$data['Logo'].'" alt="Logo">'; ?>
         </div>
         
         <br><br><br><br><br><br><br><br><br><br>
@@ -80,11 +84,11 @@ $user_id = $id;
         $current_page = $_POST['OngletNavBar'];
 
         if ($current_page === "Accueil") {
-            include 'C:\wamp64\www\ING2S2-Web/Profil_entreprises/details/accueil1.php';
+            include '../Profil_entreprises/details/accueil1.php';
         } elseif ($current_page === "A propos") {
-            include '../../Profil_entreprises/details/apropos1.php';
+            include '../Profil_entreprises/details/apropos1.php';
         } elseif ($current_page === "Posts") {
-            include 'C:\wamp64\www\ING2S2-Web/Profil_entreprises/details/posts1.php';
+            include '../Profil_entreprises/details/posts1.php';
         }
     } else {
         $current_page = "Accueil";
