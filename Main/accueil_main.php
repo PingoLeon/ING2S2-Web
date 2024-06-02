71530e90287380717b4639e984bc0afc95e572a8
Felix CADENE <felixcadene0@gmail.com>
2024-06-02 17:54:43 +0200

    Refix beug Accueil

--- Main/accueil_main.php
+++ Main/accueil_main.php
@@ -55,6 +55,15 @@
         }
     </style>
     <script>
+        function Voir_plus_fct(postId) {
+            document.getElementById('ptit_text-' + postId).style.display = 'none';
+            document.getElementById('grd_text-' + postId).style.display = 'block';
+        }
+
+        function Voir_moins_fct(postId) {
+            document.getElementById('grd_text-' + postId).style.display = 'none';
+            document.getElementById('ptit_text-' + postId).style.display = 'block';
+        }
         function toggleCommentsAndMessageBar(postId) {
             var commentsSection = document.getElementById('section-comments-display-' + postId);
             var messageBar = document.getElementById('bar-post-comment-' + postId);
@@ -182,84 +191,126 @@
                                 $lieu = $data['Lieu'];
                                 $visibility = $data['Visibility_Private'];
 
-                                echo "<div class='post-container-$post_id containerdepost' id='main_bloc' ;>";
+                                echo "<div class='post-container-$post_id containerdepost' id='main_bloc';>";
                                     echo "<div class='post-header'>";
-                                        echo '<img src="' . $photo . '" alt="Photo du Post" style="width:100px;">';
-                                        echo "<div><h3>$titre</h3><p>$date</p></div>";
-                                        if ($visibility == 0) {
-                                            echo "<p style='color: #6c757d;'>Public</p>";
+                                        echo '<div class="row" style="margin-top:10px;">';
+                                            echo '<div class="col-md-2">';
+                                                $sql_user = "SELECT * FROM utilisateur WHERE User_ID = $user_id_author;";
+                                                $result_user = mysqli_query($db_handle, $sql_user);
+                                                $data_user = mysqli_fetch_assoc($result_user);
+                                                $photo_user = $data_user['Photo'];
+                                                $photo_user = '../Photos/' . $photo_user . '';
+                                                $nom = $data_user['Nom'];
+                                                $prenom = $data_user['Prenom'];
+                                                echo '<img src="' . $photo_user . '" class="img-thumbnail" style="width:500px; margin-left:10px;">';
+                                            echo '</div>';
+                                            echo '<div class="col-md-10" style="text-align: left;">';
+                                                echo "<div class='row' style='display: flex; align-items: center; margin-left:1px;'><h6><b>$prenom $nom</b></h6>&nbsp;<h3 style='color: grey;'>•</h3>&nbsp;<h4><b>$titre</b></h4></div>";
+                                                echo "<p style='font-size: 0.8rem; color: black;'>$date</p>";
+                                                if ($visibility == 0) {
+                                                    echo "<span class='badge badge-primary'>Public</span>";
+                                                } else {
+                                                    echo "<span class='badge badge-info'>Privé</span>";
+                                                }
+                                            echo '</div>';
+                                        echo '</div>';
+                                    echo "</div>";
+                                    echo '<hr style="border-top: 3px solid #0077B5; margin-left:10px; margin-right:10px;">';
+                                    echo "<div class='post-content' style='text-align: left; margin-left: 30px; margin-right: 30px;'>";
+                                        // Limit text display
+                                        if (strlen($texte) > 100) {
+                                            $short_text = substr($texte, 0, 100);
+                                            echo "<p id='ptit_text-$post_id'>$short_text... <a href='javascript:void(0);' onclick='Voir_plus_fct($post_id)'>Voir plus...</a></p>";
+                                            echo "<p id='grd_text-$post_id' style='display:none;'>$texte <a href='javascript:void(0);' onclick='Voir_moins_fct($post_id)'>Voir moins...</a></p>";
                                         } else {
-                                            echo "<p style='color: #6c757d;'>Privé</p>";
+                                            echo "<p>$texte</p>";
                                         }
+                                        echo "<p><strong>📍Lieu:</strong> $lieu</p>";
                                     echo "</div>";
-                                    echo "<div class='post-content'>";
-                                        echo "<p>$texte</p>";
-                                        echo "<p><strong>Lieu:</strong> $lieu</p>";
+                                    //Section pour afficher en grand la photo du post
+                                    echo "<div class='post-photo' style='text-align: center;'>";
+                                        echo "<img src='$photo' class='img-fluid' alt='Photo du post' style='width: 80%; border: 2px solid black;'>";
                                     echo "</div>";
-                                    // Ajouter une section déroulante pour les commentaires
-                                    echo "<div>";
-                                        echo "<button class='btn show-comments-button' onclick='toggleCommentsAndMessageBar($post_id)'>Afficher les commentaires</button>";
-                                        echo "<div id='section-comments-display-$post_id' style='display:none' class='flex-grow-1 overflow-auto p-3 comments-section comments-container' >";
-                                            // Charger les commentaires à partir de la base de données
-                                            $sql_comments = "SELECT * FROM Commentaires WHERE Post_ID = $post_id;";
-                                            $result_comments = mysqli_query($db_handle, $sql_comments);
-                                            if (mysqli_num_rows($result_comments) == 0) {
-                                                echo "<p>Aucun commentaire pour le moment.</p>";
-                                            }else{
-                                                while ($data_comment = mysqli_fetch_assoc($result_comments)) {
-                                                    $comment_text = $data_comment['Texte'];
-                                                    $comment_date = $data_comment['DatePubli'];
-                                                    $user_id_comment = $data_comment['User_ID'];
-                                                    $sql_user = "SELECT Nom, Prenom FROM utilisateur WHERE User_ID = $user_id_comment;";
-                                                    $result_user = mysqli_query($db_handle, $sql_user);
-                                                    $data_user = mysqli_fetch_assoc($result_user);
-                                                    $nom = $data_user['Nom'];
-                                                    $prenom = $data_user['Prenom'];
-                                                    echo "<p class='comment-left'>
-                                                            <span style='font-size: 1rem; font-weight: 550; color: #6c757d;'>$prenom $nom</span>
-                                                            <br> 
-                                                            <span class='my-card-text'>
-                                                            $comment_text 
-                                                            </span>
-                                                            <br> 
-                                                            <small><small>$comment_date</small></small>
-                                                        </p>";
+                                    echo "<br>";
+
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-6">';
+                                            echo "<button class='btn btn-primary'>J'aime 💖</button>";
+                                        echo '</div>';
+                                        echo '<div class="col-md-6">';
+                                            // Ajouter une section déroulante pour les commentaires
+                                            echo "<div>";
+                                                echo "<button class='btn show-comments-button' onclick='toggleCommentsAndMessageBar($post_id)'>Afficher les commentaires</button>";
+                                                
+                                            echo "</div>";
+                                        echo '</div>';
+                                    echo '</div>';
+
+                                    echo '<div class="row">';
+                                        echo '<div class="col-md-12">';
+                                                echo "<div id='section-comments-display-$post_id' style='display:none' class='flex-grow-1 overflow-auto p-3 comments-section comments-container' >";
+                                                    // Charger les commentaires à partir de la base de données
+                                                    $sql_comments = "SELECT * FROM Commentaires WHERE Post_ID = $post_id;";
+                                                    $result_comments = mysqli_query($db_handle, $sql_comments);
+                                                    if (mysqli_num_rows($result_comments) == 0) {
+                                                        echo "<p>Aucun commentaire pour le moment.</p>";
+                                                    }else{
+                                                        while ($data_comment = mysqli_fetch_assoc($result_comments)) {
+                                                            $comment_text = $data_comment['Texte'];
+                                                            $comment_date = $data_comment['DatePubli'];
+                                                            $user_id_comment = $data_comment['User_ID'];
+                                                            $sql_user = "SELECT Nom, Prenom FROM utilisateur WHERE User_ID = $user_id_comment;";
+                                                            $result_user = mysqli_query($db_handle, $sql_user);
+                                                            $data_user = mysqli_fetch_assoc($result_user);
+                                                            $nom = $data_user['Nom'];
+                                                            $prenom = $data_user['Prenom'];
+                                                            echo "<p class='comment-left'>
+                                                                    <span style='font-size: 1rem; font-weight: 550; color: #6c757d;'>$prenom $nom</span>
+                                                                    <br> 
+                                                                    <span class='my-card-text'>
+                                                                    $comment_text 
+                                                                    </span>
+                                                                    <br> 
+                                                                    <small><small>$comment_date</small></small>
+                                                                </p>";
+                                                            }
+                                                        }
+                                                echo "</div>";
+                                            
+                                                echo " <form  id='bar-post-comment-$post_id' style='display:none' id='messageForm' class='align-items-center border-top p-3 mt-auto' method='post'>
+                                                            <div class='d-flex'>
+                                                                <input type='hidden' name='post_id' value='$post_id'>
+                                                                <input id='MessageBar' type='text' name='commentaire' class='form-control flex-grow-1 me-2' placeholder='Écrire un message' autocomplete='off' required>
+                                                                <button type='submit' class='btn btn-primary'>
+                                                                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-send' viewBox='0 0 16 16'>
+                                                                        <path d='M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z'/>
+                                                                    </svg>
+                                                                </button>
+                                                            </div>
+                                                        </form>
+                                                        ";
+                                                //! Envoyer le message
+                                                if (isset($_POST['post_id']) && isset($_POST['commentaire'])) {
+                                                    $post_id = mysqli_real_escape_string($db_handle, $_POST['post_id']);
+                                                    $comment = mysqli_real_escape_string($db_handle, $_POST['commentaire']);
+                        
+                                                    // Insérer le commentaire dans la base de données
+                                                    $sql_comment = "INSERT INTO Commentaires(Post_ID, User_ID, Texte) VALUES ('$post_id', '$user_id', '$comment')";
+                                                    $result = mysqli_query($db_handle, $sql_comment);
+                        
+                                                    if (!$result) {
+                                                        echo "Erreur: $sql_comment <br>" . mysqli_error($db_handle);
+                                                    } else {
+                                                        $_SESSION['current_post'] = $post_id;
+                                                        header("Location: ../Main/accueil_main.php");
+                                                        ob_end_flush();
+                                                        exit;
                                                     }
                                                 }
+                                            echo "</div>";
                                         echo "</div>";
-                                        echo " <form  id='bar-post-comment-$post_id' style='display:none' id='messageForm' class='align-items-center border-top p-3 mt-auto' method='post'>
-                                                    <div class='d-flex'>
-                                                        <input type='hidden' name='post_id' value='$post_id'>
-                                                        <input id='MessageBar' type='text' name='commentaire' class='form-control flex-grow-1 me-2' placeholder='Écrire un message' autocomplete='off' required>
-                                                        <button type='submit' class='btn btn-primary'>
-                                                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-send' viewBox='0 0 16 16'>
-                                                                <path d='M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z'/>
-                                                            </svg>
-                                                        </button>
-                                                    </div>
-                                                </form>
-                                                ";
-                                        //! Envoyer le message
-                                        if (isset($_POST['post_id']) && isset($_POST['commentaire'])) {
-                                            $post_id = mysqli_real_escape_string($db_handle, $_POST['post_id']);
-                                            $comment = mysqli_real_escape_string($db_handle, $_POST['commentaire']);
-                
-                                            // Insérer le commentaire dans la base de données
-                                            $sql_comment = "INSERT INTO Commentaires(Post_ID, User_ID, Texte) VALUES ('$post_id', '$user_id', '$comment')";
-                                            $result = mysqli_query($db_handle, $sql_comment);
-                
-                                            if (!$result) {
-                                                echo "Erreur: $sql_comment <br>" . mysqli_error($db_handle);
-                                            } else {
-                                                $_SESSION['current_post'] = $post_id;
-                                                header("Location: ../Main/accueil_main.php");
-                                                ob_end_flush();
-                                                exit;
-                                            }
-                                        }
-                                        echo "
-                                            </div>
-                                        </div>";
+                                echo "</div>";
+
                             }
                         }
                         
@@ -303,10 +354,6 @@
             echo '</a>';
         echo '</footer>';
     echo '</div>';
-    //Logo de l'entreprise that when clicked, a modal appears with the company's information
-    /*<a href="#" onclick="openImageModal()">
-                        <img src="../Photos/EngineerIN_logo.png" alt="ECE Paris" style="width: 60%;">
-                    </a>*/
     
     
     ?>
