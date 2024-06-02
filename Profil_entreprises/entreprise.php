0c7d2fcfe857fb867203c727fc071070b890a588
Léon <dalleleon@gmail.com>
2024-06-02 10:17:28 +0200

    Résolution de conflit

--- Profil_entreprises/entreprise.php
+++ Profil_entreprises/entreprise.php
@@ -10,6 +10,7 @@ $user_id = $id;
 <head>
 <meta charset="utf-8">
     <title>❤❤</title>
+    <link rel="icon" href="../Photos/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
